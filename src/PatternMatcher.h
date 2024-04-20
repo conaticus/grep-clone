828d564ed37db4701fcf7eeb41e949dda854f159
Sam <enquiry.conaticus@gmail.com>
2024-04-20 12:07:02 +0100

    combining character classes (decoy for codecrafters)

--- src/PatternMatcher.h
+++ src/PatternMatcher.h
@@ -4,7 +4,7 @@
 class PatternMatcher
 {
 public:
-    PatternMatcher(const std::string& input, const std::string& pattern) : input(input), pattern(pattern) {};
+    PatternMatcher(const std::string& input, const std::string& pattern) : input(input), pattern(pattern) {}
     bool Match();
 private:
     const std::string input;
